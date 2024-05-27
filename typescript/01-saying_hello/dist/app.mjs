4098af714dcee595c27c7a0645199579236b4afa
gabrieldrouin <info@gabrieldrouin.com>
2024-05-27 10:36:49 -0400

    Rename node to typescript and convert .mjs project to .mts project

---
+++ typescript/01-saying_hello/dist/app.mjs
@@ -0,0 +1,10 @@
+// ECMAScript module system (ESM) syntax
+import readline from "node:readline";
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+rl.question(`What is your name? `, (name) => {
+    console.log(`Hi ${name}, nice to meet you!`);
+    rl.close();
+});
